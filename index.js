f53b34df332082111d34b5db7f9b502989df922b
isaacrutledgewilson <devpreneurpro@proton.me>
2023-04-19 22:55:30 -0700

    fininshed lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+// 1) welcomeMessage
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// 2) excitedWelcomeMessage
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+// 3) shortGreeting
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
+
